4ce961330f238c639ef3faa53907e8d96c8d1e22
MandeepM <maharjanmemandeep@gmail.com>
2021-07-09 21:25:21 +0545

    minors

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,13 +3,13 @@ const User = require("../model/user");
 const userController = require("../controller/user.controller");
 const bcrypt = require("bcryptjs");
 const avatar = require("avatar-initials");
-const asyncHandler = require("../auth/async")
+const asyncHandler = require("../auth/async");
 // const jwt = require("jsonwebtoken");
 
 const userSignup = asyncHandler(async (req, res) => {
   const data = req.body;
   const password = data.password;
- 
+
   // const first_initial = data.fname.substring(0,1);
   // const last_initial = data.lname.substring(0,1);
   // const initials = first_initial + last_initial;
@@ -18,7 +18,7 @@ const userSignup = asyncHandler(async (req, res) => {
   // //   initials: initials,
   // //   initial_fg: '#888888',
   // //   initial_bg: '#f4f6f7',
-  // //   initial_size: 0, 
+  // //   initial_size: 0,
   // //   initial_weight: 100,
   // //   initial_font_family: "'Lato', 'Lato-Regular', 'Helvetica Neue'",
   // // });
@@ -60,29 +60,25 @@ const userLogin = asyncHandler(async (req, res, next) => {
   //because in password field we have set the property select:false , but here we need as password so we added + sign
 
   if (!user) {
-    res
-    .status(201)
-    .json({
+    res.status(201).json({
       success: false,
-      message: 'Invalid credentails user',
-    });  
+      message: "Invalid User Credentails ",
+    });
   }
 
   // const isMatch = await user.matchPassword(password); // decrypt password
-  
-  if (user.password!= password) {
-    res
-    .status(201)
-    .json({
+
+  if (user.password != password) {
+    res.status(201).json({
       success: false,
-      message: 'Invalid credentails',
+      message: "Invalid Credentails",
     });
+  } else {
+    sendTokenResponse(user, 200, res);
   }
- else{
-  sendTokenResponse(user, 200, res);
-}
 });
 
 module.exports = {
-  userSignup, userLogin
+  userSignup,
+  userLogin,
 };
