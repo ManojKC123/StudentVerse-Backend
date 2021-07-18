e3c5b6c655117db8d1ebbdc3f02cc5e7e601ed90
pSaurav10 <sauravinmail@gmail.com>
2021-07-18 14:59:57 +0545

    Minor changes

--- src/model/vote.js
+++ src/model/vote.js
@@ -4,6 +4,7 @@ const schema = mongoose.schema;
 const voteSchema = new schema({
     user: {
         type: schema.Types.ObjectId,
+        ref: 'user',
         required: true
     },
     vote:{type: Number, required: true}
