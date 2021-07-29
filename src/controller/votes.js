76da1f4bf471ad5754c030c8133ef3943f2b3e34
MandeepM <maharjanmemandeep@gmail.com>
2021-07-29 20:30:33 +0545

    vote system recheck

--- src/controller/votes.js
+++ src/controller/votes.js
@@ -1,40 +1,51 @@
 const asyncHandler = require("../auth/async");
 const Post = require("../model/post");
 
-exports.upvote = asyncHandler(async(req, res) => {
-    const { id } = req.user;
-    const poster = await Post.findById(req.body.post);
-    const answer = req.body.answer;
-    if (answer) {
-        const findAns = await poster.answer.id(req.body.answer);
-        findAns.vote(id, 1);
-        
-        const answerVote = await poster.save();
-        return res.json({success: true, message: "Up Voted Answer", data: answerVote});
-    }
+exports.upvote = asyncHandler(async (req, res) => {
+  const { id } = req.user;
+  const poster = await Post.findById(req.body.post);
+  const answer = req.body.answer;
+  if (answer) {
+    const findAns = await poster.answer.id(req.body.answer);
+    findAns.vote(id, 1);
+
+    const answerVote = await poster.save();
+    return res.json({
+      success: true,
+      message: "Up Voted Answer",
+      data: answerVote,
+    });
+  }
 });
 
+exports.downvote = asyncHandler(async (req, res) => {
+  const { id } = req.user;
+  const poster = await Post.findById(req.body.post);
+  const answer = req.body.answer;
+  if (answer) {
+    const findAns = await poster.answer.id(req.body.answer);
+    findAns.vote(id, -1);
+    const answerVote = await poster.save();
+    return res.json({
+      success: true,
+      message: "Down Voted Answer",
+      data: answerVote,
+    });
+  }
+});
 
-exports.downvote = asyncHandler(async(req,res) => {
-    const { id } = req.user;
-    const poster = await Post.findById(req.body.post);
-    const answer = req.body.answer;
-    if (answer){
-        const findAns = await poster.answer.id(req.body.answer);
-        findAns.vote(id, -1);
-        const answerVote = await poster.save();
-        return res.json({success: true, message: "Down Voted Answer", data: answerVote})
-    }
-})
-
-exports.unvote = asyncHandler(async(req,res) => {
-    const { id } = req.user;
-    const poster = await Post.findById(req.body.post);
-    const answer = req.body.answer;
-    if(answer){
-        const findAns = await poster.answer.id(req.body.answer);
-        findAns.vote(id, 0);
-        const answerVote = await poster.save();
-        return res.json({success: true, message:"Answer Vote Cancelled", data: answerVote});
-    }
-})
+exports.unvote = asyncHandler(async (req, res) => {
+  const { id } = req.user;
+  const poster = await Post.findById(req.body.post);
+  const answer = req.body.answer;
+  if (answer) {
+    const findAns = await poster.answer.id(req.body.answer);
+    findAns.vote(id, 0);
+    const answerVote = await poster.save();
+    return res.json({
+      success: true,
+      message: "Answer Vote Cancelled",
+      data: answerVote,
+    });
+  }
+});
--- src/model/answer.js
+++ src/model/answer.js
@@ -1,56 +1,67 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
-const voteSchema = require('./vote');
-const commentSchema = require('./comment');
+const voteSchema = require("./vote");
+const commentSchema = require("./comment");
 
 const answerSchema = new schema({
-    author: {
-        type: schema.Types.ObjectId,
-        ref: 'user'
-    },
-    createdAt: { type: Date, default: Date.now },
-    text: { type: String },
-    comment: [commentSchema],
-    votes: [voteSchema],
-    score: { type: Number, default: 0 }
+  author: {
+    type: schema.Types.ObjectId,
+    ref: "user",
+  },
+  createdAt: { type: Date, default: Date.now },
+  text: { type: String },
+  comment: [commentSchema],
+  votes: [voteSchema],
+  score: { type: Number, default: 0 },
 });
 
-answerSchema.set('toJSON', { getters: true });
+answerSchema.set("toJSON", { getters: true });
 
 answerSchema.methods = {
-    vote: function (user, vote) {
-        const existingVote = this.votes.find((v) => v.user._id.equals(user));
+  vote: function (user, vote) {
+    console.log("user", user);
+    const existingVote = this.votes.find((v) => {
+      console.log("vuser", v);
+      if (v.user[0].equals(user)) {
+        return v;
+      }
+      return false;
+    });
+    console.log("existingVote", existingVote);
 
-        if (existingVote) {
-            this.score -= exitstingVote.vote;
-            if (vote == 0) {
-                this.votes.pull(existingVote);
-            }
-            else {
-                this.score += vote;
-                existingVote.vote = vote;
-            }
+    if (existingVote) {
+      console.log("Youve already voted");
+      if (vote == -1) {
+        if (existingVote.vote == 1) {
+          this.score += existingVote.vote;
+          this.votes.pull(existingVote);
         }
-        else if (vote !== 0) {
-            this.score += vote;
-            this.votes.push({ user, vote });
+        return false;
+      } else if (vote == 1) {
+        if (existingVote.vote == -1) {
+          this.score += existingVote.vote;
+          this.votes.pull(existingVote);
         }
-        return this;
-    },
+        return false;
+      }
+    } else {
+      this.score += vote;
+      this.votes.push({ user, vote });
+    }
+  },
 
-    addComment: function (author, text) {
-        this.comment.push({ author, text });
-        return this;
-    },
+  addComment: function (author, text) {
+    this.comment.push({ author, text });
+    return this;
+  },
 
-    removeComment: function (id) {
-        const comment = this.comment.id(id);
-        if (!comment) throw new Error('Comment Not Found');
-        comment.remove();
-        return this;
-    },
+  removeComment: function (id) {
+    const comment = this.comment.id(id);
+    if (!comment) throw new Error("Comment Not Found");
+    comment.remove();
+    return this;
+  },
 };
-
 
 module.exports = answerSchema;
