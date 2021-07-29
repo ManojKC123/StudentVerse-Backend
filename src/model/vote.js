5f4cbb653186a9256f742912ef6eb568e615c247
pSaurav10 <sauravinmail@gmail.com>
2021-07-29 21:44:18 +0545

    Vote user was in array: Removed array

--- src/model/vote.js
+++ src/model/vote.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const voteSchema = new schema({
-    user: [{
+    user: {
         type: schema.Types.ObjectId,
         required: true
-    }],
+    },
     vote:{type: Number, required: true}
 },
 {_id: false}
