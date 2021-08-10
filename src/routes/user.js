7dd709389011b3cdb4b8bd85f66ef15129f6c230
pSaurav10 <sauravinmail@gmail.com>
2021-08-10 11:59:36 +0545

    User fetch without guard

--- src/routes/user.js
+++ src/routes/user.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => {
 router.post("/signup", userController.userSignup);
 router.post("/login", userController.userLogin);
 router.get("/profile", guard, userController.getUser);
-router.get("/user/:id", guard, userController.findUser);
+router.get("/user/:id", userController.findUser);
 router.put("/user/update", guard, userController.updateUser);
 router.put("/profile/update", guard, userController.updateUser);
 router.get("/logout", userController.Logout);
