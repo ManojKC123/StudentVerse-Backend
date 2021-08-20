f3132984e9284c22fb4967b9ffff96506b20de37
pSaurav10 <sauravinmail@gmail.com>
2021-08-20 21:16:40 +0545

    Chapter to chapterid

--- src/controller/quizs.js
+++ src/controller/quizs.js
@@ -4,8 +4,8 @@ const asyncHandler = require("../auth/async");
 const ErrorResponse = require("../auth/ErrorResponse");
 
 exports.addQuiz = asyncHandler(async (req, res, next) => {
-    if (req.params.chapter) {
-        const chapter = req.params.chapter;
+    if (req.params.chapterid) {
+        const chapter = req.params.chapterid;
         const {name, question, options, answer } = req.body;
         let quiz = await new Quiz({
             name,
@@ -33,7 +33,7 @@ exports.loadQuiz = asyncHandler(async (req, res, next) => {
 })
 
 exports.loadchapterQuiz = asyncHandler(async (req, res, next)=>{
-    const quiz = await Quiz.find({"chapter": req.params.chapter});
+    const quiz = await Quiz.find({"chapter": req.params.chapterid});
     
     res.status(200).json({
         success: true,
