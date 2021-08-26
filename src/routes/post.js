441b7b0abaeb6ee97e175613061237ebd55b1026
pSaurav10 <sauravinmail@gmail.com>
2021-08-26 14:57:20 +0545

    load other post route

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { loadPosts, loadPostsById, addPost, loadUserPost, updatePost } = require("../controller/posts");
+const { loadPosts, loadPostsById, addPost, loadUserPost, updatePost, loadOtherPost } = require("../controller/posts");
 
 const { guard } = require("../auth/auth");
 
@@ -10,5 +10,6 @@ router.get("/post/:id", loadPostsById);
 router.post("/addQuestion", guard, addPost);
 router.get("/userPost", guard, loadUserPost);
 router.put("/post/update", guard, updatePost);
+router.get("/otheruser/post", guard, loadOtherPost);
 
 module.exports = router;
